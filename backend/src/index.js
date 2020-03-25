916061b1016ee67fd52cc20c88d140fe593513ba
Gustavo Borges <gustavo.borges.carvalho@hotmail.com>
2020-03-25 16:49:01 -0300

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+app.use(cors()) // cors({origin: 'http://meuapp.com'}) determina qual aplicação front-end pode acessar o nosso back
 
 app.use(express.json());
 app.use(routes); /* Nosso expressApp vai usar os routes do arquivo routes */
