526d1582dbe68b03b92b84b6886997a55d8f437e
Gustavo Borges <gustavo.borges.carvalho@hotmail.com>
2020-03-25 21:45:49 -0300

    Page Profile

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'; // npm install 
 // Switch => garante que só uma rota será chamada por momento
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() {
     return (
@@ -11,6 +12,7 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
             </Switch>
         </BrowserRouter>
     )
